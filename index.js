5f98c23827809cd7266525440f3ea4304f0d68d3
MJProgramming <joey.meierkord@gmail.com>
2022-12-31 17:00:44 +0100

    Added functionality, bug fix in next commit

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const buttons = gridcont.querySelectorAll('button');
 const displaytop = document.getElementById('top');
 const displaybottom = document.getElementById('bottom');
 
-const check = {
-    checkNumber: /^[\d]{1,}$/,
-    checkCalc: /^$/,
-};
-
 buttons.forEach(button => {
     button.classList.add('btn');
     if (parseInt(button.textContent) > 0) {
@@ -16,23 +11,39 @@ buttons.forEach(button => {
 });
 
 let opIsClicked = false;
+let result;
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
-        if (e.target.className === "btn number") {
-            if (opIsClicked) {
-                number = displaybottom.textContent;
-                opIsClicked = false;
-                displaybottom.textContent = "";
-            }
+        if (e.target.className === "btn number" || e.target.id === "last") {
             displaybottom.textContent = displaybottom.textContent + `${e.target.textContent}`;
         } else if (e.target.className === "btn operator") {
             opIsClicked = true;
-            if (check.checkNumber.test(displaybottom.textContent)) {
-                displaytop.textContent = displaybottom.textContent.concat(" ", `${e.target.textContent}`);
+            result = checkForNums();
+            if (result !== null) {
+                displaytop.textContent = `${result} ${e.target.textContent}`;
+                result = null;
+                return;
             }
+            displaytop.textContent += `${displaybottom.textContent} ${e.target.textContent}`;
+            displaybottom.textContent = "";
+        } else if (e.target.id === "dot") {
+            displaybottom.textContent += ".";
         }
     });
 });
+
+
+
+function checkForNums() {
+    let operation = [];
+    let withNum;
+    if (displaytop.textContent !== "" && displaybottom.textContent !== "") {
+        operation = displaytop.textContent.split(" ");
+        withNum = displaybottom.textContent;
+        return operate(operation[1], operation[0], withNum);
+    }
+    return null;
+}
 
 function add(number, anNumber) {
     return number + anNumber;
@@ -51,18 +62,25 @@ function divide(number, anNumber) {
 }
 
 function operate(operator, number, anNumber) {
+    if (String(number).includes(".") || String(anNumber).includes(".")) {
+        number = parseFloat(number);
+        anNumber = parseFloat(anNumber);
+    } else {
+        number = parseInt(number);
+        anNumber = parseInt(anNumber);
+    }
+
     if (operator === "+") {
-        return add(number, anNumber);
+        return Math.round((add(number, anNumber) + Number.EPSILON) * 100) / 100;
     } else if (operator === "-") {
-        return substract(number, anNumber);
+        return Math.round((substract(number, anNumber) + Number.EPSILON) * 100) / 100;
     } else if (operator === "÷" || operator === "/") {
         if (anNumber === 0) {
-            alert('Division by zero');
             return NaN;
         }
-        return divide(number, anNumber);
+        return Math.round((divide(number, anNumber) + Number.EPSILON) * 100) / 100;
     } else if(operator === "*" || operator === "x") {
-        return multiply(number, anNumber);
+        return Math.round((multiply(number, anNumber) + Number.EPSILON) * 100) / 100;
     } else {
         return null;
     }
