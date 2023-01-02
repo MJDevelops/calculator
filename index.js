e5e29c5b9e4820f64770850b08c34af61940b2ac
MJProgramming <joey.meierkord@gmail.com>
2023-01-02 22:37:05 +0100

    Implemented full version of calculator, new keyboard support, added design

--- index.js
+++ index.js
@@ -10,29 +10,82 @@ buttons.forEach(button => {
     }
 });
 
-let opIsClicked = false;
 let result;
+let content = [];
+
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         if (e.target.className === "btn number" || e.target.id === "last") {
-            displaybottom.textContent = displaybottom.textContent + `${e.target.textContent}`;
+            setNumbers(e.target);
         } else if (e.target.className === "btn operator") {
-            opIsClicked = true;
-            result = checkForNums();
-            if (result !== null) {
-                displaytop.textContent = `${result} ${e.target.textContent}`;
-                result = null;
-                return;
-            }
-            displaytop.textContent += `${displaybottom.textContent} ${e.target.textContent}`;
-            displaybottom.textContent = "";
+           getPartResult(e.target);
         } else if (e.target.id === "dot") {
-            displaybottom.textContent += ".";
+            appendPoint();
+        } else if (e.target.className === "btn del") {
+            removeLast();
+        } else if (e.target.className === "btn ac") {
+            displaytop.textContent = "";
+            displaybottom.textContent = "";
+        } 
+        else if (e.target.id === "equal") {
+            completeResult();
         }
     });
 });
 
+document.addEventListener('keyup', (e) => {
+    if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "*") {
+        getPartResult(e);
+    } else if (e.key >= 0 && e.key <= 9) {
+        setNumbers(e);
+    } else if (e.key === ".") {
+        appendPoint();
+    } else if (e.key === "Backspace") {
+        removeLast();
+    } else if (e.key === "Enter" || e.key === "=") {
+        completeResult();
+    }
+});
 
+function setNumbers(e) {
+    if (displaytop.textContent.includes("=")) {
+        displaytop.textContent = "";
+    }
+    return e.key >= 0 && e.key <= 9 ? displaybottom.textContent += `${e.key}` : displaybottom.textContent += `${e.textContent}`;
+}
+
+function getPartResult(e) {
+    let type;
+    let keyPress;
+    if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "*") {
+        type = "keyboard"
+        if (e.key === "*") {
+            keyPress = "x";
+        } else if (e.key === "/") {
+            keyPress = "÷";
+        } else {
+            keyPress = e.key;
+        }
+    } else {
+        type = "button";
+    }
+
+    result = checkForNums();
+    if (result === null && parseInt(displaytop.textContent) === "0" && (keyPress === "÷" || e.textContent === "÷")) {
+        alert("Hey, you! You can't divide by zero!");
+    } else if (displaytop.textContent.includes("=")) {
+        content = displaytop.textContent.split(" ");
+        displaytop.textContent = type === "button" ? `${content[content.length - 1]} ${e.textContent}` : `${content[content.length - 1]} ${keyPress}`;
+        displaybottom.textContent = "";
+    } else if (displaybottom.textContent !== "") {
+        displaytop.textContent = type === "button" ? `${displaybottom.textContent} ${e.textContent}` : `${displaybottom.textContent} ${keyPress}`;
+        displaybottom.textContent = "";
+    } else {
+        content = displaytop.textContent.split(" ");
+        content[1] = type === "button" ? e.textContent : keyPress;
+        displaytop.textContent = content.join(" ");
+    }
+}
 
 function checkForNums() {
     let operation = [];
@@ -43,6 +96,34 @@ function checkForNums() {
         return operate(operation[1], operation[0], withNum);
     }
     return null;
+}
+
+function appendPoint() {
+    if (!(displaybottom.textContent.includes("."))) {
+        displaybottom.textContent += ".";
+    }
+}
+
+function removeLast() {
+    if (displaybottom.textContent !== "") {
+        content = displaybottom.textContent.split("");
+        content.pop();
+        displaybottom.textContent = content.join("");
+    }
+}
+
+function completeResult() {
+    if (displaybottom.textContent !== "") {
+        result = checkForNums();
+        if (result !== null) {
+            content = displaytop.textContent.split(" ");
+            content.push(displaybottom.textContent, "=", result);
+            displaybottom.textContent = "";
+            displaytop.textContent = content.join(" ");
+        } else {
+            alert("Hey, you! You can't divide by zero!");
+        }
+    }
 }
 
 function add(number, anNumber) {
@@ -76,7 +157,7 @@ function operate(operator, number, anNumber) {
         return Math.round((substract(number, anNumber) + Number.EPSILON) * 100) / 100;
     } else if (operator === "÷" || operator === "/") {
         if (anNumber === 0) {
-            return NaN;
+            return null;
         }
         return Math.round((divide(number, anNumber) + Number.EPSILON) * 100) / 100;
     } else if(operator === "*" || operator === "x") {
